a8e511e3a407207c7dbc740c634dbc068982164c
shimy <avstoev@gmail.com>
2021-11-30 18:06:08 +0200

    project has been initialized

---
+++ src/test/java/com/example/bsweb/BSwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bsweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BSwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
