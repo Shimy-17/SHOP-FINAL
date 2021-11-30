5b897aee85963a6ee91966fa947a75546469b8dc
shimy <avstoev@gmail.com>
2021-11-30 17:46:31 +0200

    project has been initialized

---
+++ src/main/java/com/example/bsweb/BSwebApplication.java
@@ -0,0 +1,14 @@
+package com.example.bsweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BSwebApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(BSwebApplication.class, args);
+	}
+
+}
