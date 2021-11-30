5d8b98b9854774cbd7ac7dfadc177155beb163a4
shimy <avstoev@gmail.com>
2021-11-30 18:46:25 +0200

    empty spaces have been deleted

--- src/main/java/com/example/bsweb/BSwebApplication.java
+++ src/main/java/com/example/bsweb/BSwebApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BSwebApplication {
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(BSwebApplication.class, args);
 	}
-
 }
